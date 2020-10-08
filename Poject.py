04959163a2043dec073be370d172dc721285f20b
Roberto Rivera Terán <33244698+11081999@users.noreply.github.com>
2020-10-08 10:05:46 -0500

    f
    f

--- Poject.py
+++ Poject.py
@@ -1,32 +1,48 @@
 
-from graphviz import * as graph
+#from graphviz import * as graph
+with open('test1.txt') as f:
+    first_line = f.readline()
+
+
 
 #https://stackoverrun.com/es/q/3344316
 
 
 #Arrays
 #Array= []
-#set1.append(set[i])
+#set1.append(set[i])s
 
 #Strings
 #String[1]
 #len(string)
 #string.find()
 
+
 #Recoger valores de la primera fila del .txt y ordenarlos en un array (q0, q1, q2, q3)
-nodos= []
+nodos= [0, 0 , 0]
+
+#set = [0 for i in range(1, int(input1) + 1)]
+
+print(len(first_line))
+
+nodos * 4 = 12
+
+nodos[0]= (first_line[0] + first_line[1])
+
+
+print(nodos)
 
 #Recoger los valroes de la segunda fila del .txt y agregarlos a un array (a,b)
-valores= []
+#valores= []
 
 #Declarar la variable del estado inicial
-init= "la linea tres del .txt"
+#init= "la linea tres del .txt"
 
 #Dclarar los lygares en donde es estado final
-final= []
+#final= []
 
 #Make transition table basado en los valores de la quinta linea hasta n
-transTable= [][]
+#transTable= [][]
 
 #De la tabla de transicion buscar valores exactamente iguales (por fila) de los nodos y los valores
 
