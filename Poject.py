2baee12a27d31534a97d219c491073a2bb09f57e
Roberto Rivera Terán <33244698+11081999@users.noreply.github.com>
2020-10-08 09:39:35 -0500

    update
    }upfate

--- Poject.py
+++ Poject.py
@@ -2,3 +2,34 @@
 from graphviz import * as graph
 
 #https://stackoverrun.com/es/q/3344316
+
+
+#Arrays
+#Array= []
+#set1.append(set[i])
+
+#Strings
+#String[1]
+#len(string)
+#string.find()
+
+#Recoger valores de la primera fila del .txt y ordenarlos en un array (q0, q1, q2, q3)
+nodos= []
+
+#Recoger los valroes de la segunda fila del .txt y agregarlos a un array (a,b)
+valores= []
+
+#Declarar la variable del estado inicial
+init= "la linea tres del .txt"
+
+#Dclarar los lygares en donde es estado final
+final= []
+
+#Make transition table basado en los valores de la quinta linea hasta n
+transTable= [][]
+
+#De la tabla de transicion buscar valores exactamente iguales (por fila) de los nodos y los valores
+
+#Pedir un input, y imprimir si ese imput (string) es aceptado por el DFA
+
+#De la tabla minimizada imprimir el arbol y poder pedir inut y decir si es aceptado por el DFA
